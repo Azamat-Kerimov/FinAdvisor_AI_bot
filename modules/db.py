0e588cf92951220c4319229542f206f8dbaf323b
Azamat-Kerimov <azamat@kerimov.tech>
2025-11-21 19:24:30 +0300

    Update db.py

--- modules/db.py
+++ modules/db.py
@@ -1,36 +1,18 @@
-import os
 import asyncpg
-from dotenv import load_dotenv
-import psycopg2
-from psycopg2.extras import RealDictCursor
-
-load_dotenv()
+import os
 
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = int(os.getenv("DB_PORT", 5432))
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 
-_pool = None
-
 async def create_db_pool():
-    global _pool
-    if _pool is None:
-        _pool = await asyncpg.create_pool(
-            user=DB_USER,
-            password=DB_PASSWORD,
-            database=DB_NAME,
-            host=DB_HOST,
-            port=DB_PORT,
-            min_size=1,
-            max_size=8
-        )
-    return _pool
-
-async def get_pool():
-    if _pool is None:
-        raise RuntimeError("Database pool is not initialized")
-    return _pool
+    # Создание пула подключений к базе данных PostgreSQL
+    return await asyncpg.create_pool(
+        user=DB_USER,
+        password=DB_PASSWORD,
+        database=DB_NAME,
+        host=DB_HOST,
+        port=DB_PORT
+    )
