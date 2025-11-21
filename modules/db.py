178155e867c442d6b5ecd32bcaff702381f1f3f1
Azamat-Kerimov <azamat@kerimov.tech>
2025-11-21 19:38:10 +0300

    Update db.py

--- modules/db.py
+++ modules/db.py
@@ -1,18 +1,15 @@
+# modules/db.py
 import asyncpg
 import os
 
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
+db = None
 
 async def create_db_pool():
-    # Создание пула подключений к базе данных PostgreSQL
-    return await asyncpg.create_pool(
-        user=DB_USER,
-        password=DB_PASSWORD,
-        database=DB_NAME,
-        host=DB_HOST,
-        port=DB_PORT
+    global db
+    db = await asyncpg.create_pool(
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
     )
