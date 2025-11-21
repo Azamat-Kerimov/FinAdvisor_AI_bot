d0e3c24e9f1d04a3a6ea33a0b81114243857447e
Azamat-Kerimov <azamat@kerimov.tech>
2025-11-21 16:04:23 +0300

    Update db.py

--- modules/db.py
+++ modules/db.py
@@ -1,32 +1,26 @@
 import os
 import asyncpg
 from dotenv import load_dotenv
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 load_dotenv()
 
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = int(os.getenv("DB_PORT", 5432))
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
 
-_pool = None
+def create_db_pool():
 
-async def create_db_pool():
-    global _pool
-    if _pool is None:
-        _pool = await asyncpg.create_pool(
-            user=DB_USER,
-            password=DB_PASSWORD,
-            database=DB_NAME,
-            host=DB_HOST,
-            port=DB_PORT,
-            min_size=1,
-            max_size=8
-        )
-    return _pool
+    return psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT,
+        cursor_factory=RealDictCursor
+    )
 
-async def get_pool():
-    if _pool is None:
-        raise RuntimeError("Database pool is not initialized")
-    return _pool
+
